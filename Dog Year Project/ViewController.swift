a0db2ab738791e5877c1b41cc27d6e96e548950b
Jordaleste <charlie@esnenterprises.com>
2015-01-14 09:00:48 -0500

    Initial Commit

---
+++ Dog Year Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dog Year Project
+//
+//  Created by Charles Eison on 1/14/15.
+//  Copyright (c) 2015 Charles Eison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
