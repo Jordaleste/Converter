5cc1ece0f9c1dee045802ccca27ed7f755bef68f
Jordaleste <charlie@esnenterprises.com>
2015-01-14 11:13:58 -0500

    changed button pressed name

--- Dog Year Project/ViewController.swift
+++ Dog Year Project/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var convertButton: UIButton!
     
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func pressToConvert(sender: UIButton) {
+    @IBAction func convertButtonPressed(sender: UIButton) {
+    
+    
         
         let enteredHumanYears = Double((humanYears.text as NSString).doubleValue)
         let conversionFactor = 7.0
@@ -38,8 +41,8 @@ class ViewController: UIViewController {
         humanYears.resignFirstResponder()
         convertButton.setTitle("Lets do it again!", forState: UIControlState.Normal)
         
-        
     }
+    
     
 
 
