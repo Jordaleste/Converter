921cc1f369f5cb88525972fa66ec0a133dec5a38
Jordaleste <charlie@esnenterprises.com>
2015-01-14 10:27:59 -0500

    First Commit

--- Dog Year Project/ViewController.swift
+++ Dog Year Project/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var answerDogYears: UILabel!
+    
+    @IBOutlet weak var humanYears: UITextField!
+    
+    @IBOutlet weak var convertButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +26,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pressToConvert(sender: UIButton) {
+        
+        let enteredHumanYears = Double((humanYears.text as NSString).doubleValue)
+        let conversionFactor = 7.0
+        answerDogYears.hidden = false
+        answerDogYears.text = "\(enteredHumanYears * conversionFactor)" + " Dog years old!"
+        answerDogYears.textColor = UIColor.blueColor()
+        humanYears.text = ""
+        humanYears.resignFirstResponder()
+        convertButton.setTitle("Lets do it again!", forState: UIControlState.Normal)
+        
+        
+    }
+    
 
 
 }
